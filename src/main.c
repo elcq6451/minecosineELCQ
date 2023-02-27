83d9f5a53f3f5378f13f5331af6722c673079683
elcq6451 <elcq6451@connahsquayhigh.co.uk>
2023-02-27 22:51:03 +0000

    base added

---
+++ src/main.c
@@ -0,0 +1,31 @@
+#include <fxcg/display.h>
+#include <fxcg/keyboard.h>
+
+void main(void) {
+    int key;
+    int WIDTH;
+     
+    Bdisp_AllClr_VRAM();
+    SetBackGround(1);
+    PrintXY(1, 1, "  Hello!", TEXT_MODE_NORMAL, TEXT_COLOR_RED);
+
+
+    // set pixel at a coordinate
+    int y = 150;
+    int x = 30;
+    unsigned  short col = 2;
+    unsigned short *v = GetVRAMAddress();
+    v+=y*384+x;
+    *v=col;
+    
+
+    while (1) {
+        GetKey(&key);
+
+        if (key == KEY_CTRL_EXE) {
+            break;
+        }
+    }
+ 
+    return;
+}
